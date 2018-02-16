32e76042f2d73b3309b970e3eba5ed8f38d9bdbe
jamiefox <jamiefox.fox3@gmail.com>
2018-02-15 20:00:29 -0600

    first commit

---
+++ SeriesOfNumbers/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace SeriesOfNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "";
+            int[] numbersArray = null;
+
+            Console.WriteLine("Enter a series of number separated by a comma:");
+            input = Console.ReadLine();
+
+            try
+            {
+                // see: https://www.dotnetperls.com/exception
+                // This try catch helps in handling exceptions, we can display messages in the catch block when the user forgets to provide the correct input
+                numbersArray = Array.ConvertAll(input.Split(','), int.Parse); // see: https://www.dotnetperls.com/array-convertall for `Array.ConvertAll`
+                int maxValue = numbersArray.Max();
+                Console.WriteLine("The max value is: " + maxValue);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                Console.WriteLine("***Did you forget to only include numbers?***");
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
